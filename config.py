3c2ebc523f3dd54c4aca5353729ca82dac2018d0
sachinopboy2 <sachin25u7@gmail.com>
2025-11-01 15:49:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI")
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID"))
@@ -40,8 +40,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+0nICIHfE2AkxMzA1")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+0nICIHfE2AkxMzA1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/RAGNOR_NETWORK")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/internalxfeelings")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
